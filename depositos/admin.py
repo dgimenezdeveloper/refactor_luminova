7aa87c9cd9ccf7b26152373b136865ee52224b27
Dario <dgimenez.developer@gmail.com>
2025-07-24 08:51:16 -0300

    Se registra Depósito y StockProductoTerminado en el admin

--- depositos/admin.py
+++ depositos/admin.py
@@ -1,1 +1,13 @@
-# Admin de la app depósitos
+
+from django.contrib import admin
+from .models import Deposito, StockProductoTerminado
+
+@admin.register(Deposito)
+class DepositoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "ubicacion", "descripcion")
+    search_fields = ("nombre", "ubicacion")
+
+@admin.register(StockProductoTerminado)
+class StockProductoTerminadoAdmin(admin.ModelAdmin):
+    list_display = ("producto", "deposito", "cantidad")
+    list_filter = ("deposito", "producto")
